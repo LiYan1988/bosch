62824f35c94ca5274c27488694154b96a6be8696
Li Yan <li.yan.ly414@gmail.com>
2016-11-05 09:40:41 +0100

    Find good categorical features

--- bosch5featureMerging.py
+++ bosch5featureMerging.py
@@ -18,15 +18,18 @@ cols= list(cols.columns)
 cols.remove('Id')
 Response = pd.read_csv('input/train_numeric.csv', usecols=['Response'], 
                        dtype=np.float16)
-feat_cat_cnt = []
-#feat_cat_frq = []
+feat_cat_stat = []
 for i, c in enumerate(cols):
     feat_cat = pd.read_csv('input/train_categorical.csv', usecols=[c], 
                            dtype=np.str)
     feat_cat['Response'] = Response.values
-    feat_cat_cnt.append(feat_cat.groupby(c)['Response'].count().reset_index())
-    print('Processed column {}, {}, {} positive responses'.format(i, c, 
-          feat_cat_cnt[-1].Response.sum()))
-#    feat_cat_frq.append(feat_cat.groupby(c)['Response'].mean())
+    cnt = feat_cat.groupby(c)['Response'].count().reset_index()
+    cnt.columns = [c, 'Count']
+    pos = feat_cat.groupby(c)['Response'].sum().reset_index()
+    cnt['Sum'] = pos['Response']
+    cnt['Frequency'] = cnt['Sum'].div(cnt['Count'], axis=0)
+    feat_cat_stat.append(cnt)
+    print('Processed column {}, {}, {} events, {} positives'.format(i, c, 
+          feat_cat_stat[-1].Count.sum(), feat_cat_stat[-1].Sum.sum()))
 #    if i > 2:
 #        break
